ebc59709c11bb46c05cae78aa9bb943ad133908d
Shiran Levy <shiranox@gmail.com>
2021-12-12 18:00:18 +0200

    Implementing Create User via Bit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './store/index';
+
 
 ReactDOM.render(
-  <Provider store={store}> 
-      <App />
-  </Provider>,
+      <App />,
   document.getElementById('root')
 );
 
