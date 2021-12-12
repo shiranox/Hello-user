83268e48b108a6292e5d775d74e88e8150f03c05
Shiran Levy <shiranox@gmail.com>
2021-12-12 18:30:07 +0200

    Implementing Create user via bit

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require ('mongoose');
-const Schema = mongoose.Schema;
-const userSchema = new Schema({
-  username: String,
-  password: String,
-});
-//model:
-const user = mongoose.model('user', userSchema);
-module.exports = user;
